3b4a29f5fe806660f5df2f2b85aca971c0cde271
suranyasunkavilli27 <suranyasunkavilli@gmail.com>
2020-07-11 11:15:26 +0530

    first commit

---
+++ src/main/java/DataStreams/com/DataStreams/App.java
@@ -0,0 +1,13 @@
+package DataStreams.com.DataStreams;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/DataStreams/com/DataStreams/Average.java
@@ -0,0 +1,22 @@
+package DataStreams.com.DataStreams;
+import java.util.*;
+
+public class Average {
+	public static double average(List<Integer> list){
+		return list.stream().mapToInt(i -> i).average().getAsDouble();
+	}
+	
+	public static void main(String args[]){
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		ArrayList<Integer>list1 = new ArrayList<>();
+		for(int i = 0; i < n; i++) {
+			list1.add(in.nextInt());
+		}
+		double res=average(list1);
+		System.out.println(res);
+		in.close();
+
+	}
+
+}
